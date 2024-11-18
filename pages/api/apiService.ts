2802f65b1c75f7550304f2cf596117223853e318
runsys-kf <k-fujishima@runsystem.co.jp>
2024-11-18 14:57:03 +0900

    TryExceptを外して詳細なエラー出力をチェック

--- pages/api/apiService.ts
+++ pages/api/apiService.ts
@@ -21,36 +21,36 @@ console.log("API_ENDPOINTS", API_ENDPOINTS);
 //APIリクエスト関数
 export const fetchData = async (endpoint: string, data: any, router: NextRouter) => {
   console.log("endpoint: ", endpoint);
-  try {
+  //try {
 
-    let url = "";
+  let url = "";
 
-    // ログイン認証
-    if (endpoint === "login") {
-        url = "https://loginapi-atgue5hbdugadzf2.z01.azurefd.net/api/login";
-    }
-    // 店舗別データ表示
-    if (endpoint === "display_by_store") {
-        url = "https://displaybystore-h8aagzbhegc6d7ch.z01.azurefd.net/api/display_by_store";
-    }
-    // 日別データ表示
-    if (endpoint === "display_by_date") {
-        url = "https://displaybystore-h8aagzbhegc6d7ch.z01.azurefd.net/api/display_by_store";
-    }
-
-    console.log("endpoint and url: ", endpoint);
-
-    const response = await axios.post(url, data, {
-      withCredentials: true
-    });
-    
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-
-    if (error.response && error.response.status === 401) {
-      router.push('/login');
-    }
-    throw error;
+  // ログイン認証
+  if (endpoint === "login") {
+      url = "https://loginapi-atgue5hbdugadzf2.z01.azurefd.net/api/login";
   }
+  // 店舗別データ表示
+  if (endpoint === "display_by_store") {
+      url = "https://displaybystore-h8aagzbhegc6d7ch.z01.azurefd.net/api/display_by_store";
+  }
+  // 日別データ表示
+  if (endpoint === "display_by_date") {
+      url = "https://displaybystore-h8aagzbhegc6d7ch.z01.azurefd.net/api/display_by_store";
+  }
+
+  console.log("endpoint and url: ", endpoint);
+
+  const response = await axios.post(url, data, {
+    withCredentials: true
+  });
+  
+  return response.data;
+  //} catch (error) {
+  //  console.error('Error fetching data:', error);
+
+  //  if (error.response && error.response.status === 401) {
+  //    router.push('/login');
+  //  }
+  //  throw error;
+  //}
 };
