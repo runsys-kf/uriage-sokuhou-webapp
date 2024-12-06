b2c67c45024596cb93e267a641c3b43a558bbae6
k-komatsu <k-komatsu@runsystem.co.jp>
2024-12-06 12:32:43 +0900

    小松　閉店店舗ラジオボタン無効、〆虎削除

--- data/shopData.ts
+++ data/shopData.ts
@@ -89,5 +89,4 @@ export const initialStores = [
     { name: "札幌西町", id: "1839", prefecture: "北海道" },
     { name: "札幌清田", id: "1844", prefecture: "北海道" },
     { name: "室蘭中島サンプラザ", id: "1322", prefecture: "北海道" },
-    { name: "築地虎杖 〆虎", id: "9999", prefecture: "未指定" }
 ];
