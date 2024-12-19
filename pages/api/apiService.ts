d52b109b5e2e647c562e1dd102d3e31fc8620675
runsys-kf <k-fujishima@runsystem.co.jp>
2024-12-19 19:02:11 +0900

    Revert "ダウンロードAPIデバックプリント追加"
    This reverts commit be7fc27ae87979527d49ee3ca90f3e3ae346c825.

--- pages/api/apiService.ts
+++ pages/api/apiService.ts
@@ -81,7 +81,6 @@ export const fetchData = async (endpoint: string, data: any, router: NextRouter)
 
       const text = await blob.text();
       console.log("CSV Content:", text);
-    }
 
     return response.data;
   } catch (error) {
