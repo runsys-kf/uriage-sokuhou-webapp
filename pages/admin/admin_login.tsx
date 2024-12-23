db97627d2cad4c97f8a5d9ee9386280bf54271c0
runsys-kf <k-fujishima@runsystem.co.jp>
2024-12-23 14:53:25 +0900

    管理者ログインエンドポイント修正

--- pages/admin/admin_login.tsx
+++ pages/admin/admin_login.tsx
@@ -46,7 +46,7 @@ const AdminLoginPage = () => {
         router.push("/admin");
         return;
       } else {
-        response = await axios.post('https://adminlogin-hxcxe2dxcchehvh3.z01.azurefd.net/api/admin/admlogin',
+        response = await axios.post('https://adminlogin-hxcxe2dxcchehvh3.z01.azurefd.net/api/admlogin',
           { username, password },
           {
             headers: {
